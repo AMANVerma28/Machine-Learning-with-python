bbc68d67352629b660b71336613b2af062be51a8
AMANVerma28 <amansingh.v15@iiits.in>
2018-02-26 21:53:41 +0530

    knn_scratch

--- knn_scratch.py
+++ knn_scratch.py
@@ -15,10 +15,10 @@ def k_nearest_neighbours(data, predict, k=3):
             distances.append([euclidean_distance, group])
 
     votes = [i[1] for i in sorted(distances)[:k]]
-    #print(Counter(votes).most_common(1))
     vote_result = Counter(votes).most_common(1)[0][0]
+    confidence = Counter(votes).most_common(1)[0][1] / k
 
-    return vote_result
+    return vote_result, confidence
 
 df = pd.read_csv('Data/breast-cancer-wisconsin.txt')
 df.replace('?', -99999, inplace=True)
@@ -26,7 +26,7 @@ df.drop(['id'], 1, inplace=True)
 full_data = df.astype(float).values.tolist()
 random.shuffle(full_data)
 
-test_size = 0.2
+test_size = 0.4
 train_set = {2:[], 4:[]}
 test_set = {2:[], 4:[]}
 train_data = full_data[:-int(test_size*len(full_data))]
@@ -43,9 +43,11 @@ total = 0
 
 for group in test_set:
     for data in test_set[group]:
-        vote = k_nearest_neighbours(train_data, data, k=5)
+        vote, confidence = k_nearest_neighbours(train_set, data, k=5)
         if group == vote:
             correct += 1
+        else:
+            print(confidence)
         total += 1
 
 print('Accuracy : ', correct/total)
