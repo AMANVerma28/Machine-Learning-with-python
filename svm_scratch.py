7b08386a2324400bfe90f04822f93860b1087e5b
AMANVerma28 <amansingh.v15@iiits.in>
2018-03-12 23:18:59 +0530

    svm from scratch

--- svm_scratch.py
+++ svm_scratch.py
@@ -53,7 +53,24 @@ class Support_Vector_Machine:
     def predict(self, features):
         # sign( x.w + b )
         classification = np.sign(np.dot(np.array(features), self.w) + self.b)
+        if classification!=0 and self.visualization:
+            self.ax.scatter(features[0], features[1], s=200, marker=*, c=self.colors[classification])
         return classification
+
+    def visualize(self):
+        [[self.ax.scatter(x[0], x[1], s=100, color = self.colors[i]) for x in data_dict[i]] for i in data_dict]
+
+        # hyperplane = w.x+b
+        # v = w.x+b
+        # psv = 1
+        # nsv = -1
+        # dec = 0
+        def hyperplane(x,w,b,v):
+            return (-w[0]*x-b+v) / w[1]
+
+        datarange = (self.min_feature_value*0.9, self.max_feature_value*1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
 
 
 data_dict = {-1:np.array([[1,7],
